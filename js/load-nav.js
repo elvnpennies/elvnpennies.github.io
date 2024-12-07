fd0ed07dd248cb73252bbbf83d9a217c1858045a
yankscally <yankscally@gmail.com>
2024-12-07 23:42:30 +0000

    looks better.

--- js/load-nav.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Use relative path from root
-    fetch('/elvnpennies.github.io/components/nav.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('nav-placeholder').innerHTML = data;
-        });
-});
